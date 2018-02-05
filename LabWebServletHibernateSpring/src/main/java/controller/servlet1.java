d171ace591795b8994cab9a06e21dba562a67cb3
蒲怡仁 <james.pu@EEIT>
2018-02-05 11:48:36 +0800

    第二次Commit
    新增Servlet1

---
+++ LabWebServletHibernateSpring/src/main/java/controller/servlet1.java
@@ -0,0 +1,26 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/servlet1")
+public class servlet1 extends HttpServlet {
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served servlet1 at: ").append(request.getContextPath());
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		this.doGet(request, response);
+	}
+
+}
--- LabWebServletHibernateSpring/src/main/webapp/index.jsp
+++ LabWebServletHibernateSpring/src/main/webapp/index.jsp
@@ -15,6 +15,8 @@
 
 <h3><a href="<c:url value="/secure/login.jsp" />">Login</a></h3>
 <h3><a href="<c:url value="/pages/product.jsp" />">Poduct</a></h3>
+<h3><a href="<c:url value="/servlet1" />">servlet1</a></h3>
+
 
 </body>
 </html>
